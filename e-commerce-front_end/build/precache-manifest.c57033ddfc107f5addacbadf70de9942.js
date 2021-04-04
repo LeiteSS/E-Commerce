3cfa4a55e99339681135e7f39edf9fbfc6c9f0b9
Silas Santos Leite <estoudente.program@gmail.com>
2021-04-04 11:25:06 -0300

    Proposta de Front-End

---
+++ e-commerce-front_end/build/precache-manifest.c57033ddfc107f5addacbadf70de9942.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "265c69fd47c141da9beb742f6ce09f01",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f82592fc9474da8ac152",
+    "url": "/static/js/2.2cb1d931.chunk.js"
+  },
+  {
+    "revision": "70af1bffd51a7281193fa8f9b7319597",
+    "url": "/static/js/2.2cb1d931.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa5a661161b67d98b501",
+    "url": "/static/js/main.ae610486.chunk.js"
+  },
+  {
+    "revision": "ed8ed874e871633992a1",
+    "url": "/static/js/runtime-main.a418149b.js"
+  },
+  {
+    "revision": "63713964c07d53e198677e8b32ccc12b",
+    "url": "/static/media/commerce.63713964.png"
+  }
+]);
